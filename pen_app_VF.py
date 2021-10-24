aa9b55e431f3850b7a25bde668fba599e1c325e1
HAMZA OULAARBI <80227876+HamzaOulaarbi@users.noreply.github.com>
2021-10-24 22:09:40 +0200

    Add files via upload

--- pen_app_VF.py
+++ pen_app_VF.py
@@ -10,19 +10,18 @@ Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/
 """)
 st.sidebar.header('User Input Features')
 st.sidebar.markdown('''
-[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
+[Example CSV input file](https://github.com/HamzaOulaarbi/penguins_heroku/blob/main/Data_Exemple_penguoins.csv)
 ''')
 
 # user inputs features into DataFrame
 uploaded_file=st.sidebar.file_uploader('Upload your input CSV file',type='csv')
-
 def user_input_features():
     island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
-    sex = st.sidebar.selectbox('Sex',('male','female'))
+    sex = st.sidebar.radio('Sex',('male','female'))
     bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
     bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
     flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-    body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
+    body_mass_g =st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
     data={'island':island,
           'bill_length_mm':bill_length_mm,
           'bill_depth_mm': bill_depth_mm,
@@ -31,7 +30,7 @@ def user_input_features():
           'sex': sex}
     return pd.DataFrame([data])
 if uploaded_file:
-    input_df=pd.read_csv(uploaded_file,delimiter=';')
+    input_df=pd.read_csv(uploaded_file,delimiter=',')
 else:
     input_df=user_input_features()
 
